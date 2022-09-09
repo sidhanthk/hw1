589955fa3b7d985f325119c87a398cf54d1c8458
Sidhanth Kapila <skapila@usc.edu>
2022-09-09 18:42:36 +0000

    add + update

--- ulliststr.cpp
+++ ulliststr.cpp
@@ -14,11 +14,11 @@ ULListStr::~ULListStr()
 {
   
   clear();
-  Item* current = head_;
-  while (current != NULL){
-    delete current;
-    current = current->next;
-  }
+  // Item* current = head_;
+  // while (current != NULL){
+  //   delete current;
+  //   current = current->next;
+  // }
   //delete this;
 }
 
@@ -37,41 +37,19 @@ size_t ULListStr::size() const
 std::string* ULListStr::getValAtLoc(size_t loc) const{
   std::string* temp = NULL;
   if (loc > size_-1){
-    // throw an exception
     throw std::invalid_argument("getValAtLoc: Bad location");
   }
   Item* current = head_;
   while (current != NULL){
-    if ( (current->last - current->first) >= loc){
+    if ( (current->last - current->first) > loc){
       return &(current->val[current->first + loc]);
     }
     else{
       loc = loc - (current->last - current->first);
-      current = current -> next;
+      current = current->next;
     }
   }
-   return temp;
-   
-  // std::string* temp = NULL;
-  // if (loc > size_-1){
-  //   return temp;
-  // }
-  // Item* current = head_;
-  // while (current != NULL){
-  //   for (int i = 0; i < 10; i++){
-  //     unsigned int counter = 0;
-  //     if ( !(current->val[i] == " ")){
-  //       counter ++;
-  //       if (counter == loc){
-  //         return &current->val[i];
-  //       }
-  //     }
-  //   }
-  //   current = current->next;
-  // }
-  // return temp;
-
-  
+   return temp;  
 }
 
 void ULListStr::push_back(const std::string& val){
@@ -86,34 +64,23 @@ void ULListStr::push_back(const std::string& val){
       item1->prev = NULL;
       item1->next = NULL;
       size_ = 1;
-      //delete item1;
     }
 
     else{
       //!(tail_->val[tail_->last-1].empty())
       if ( tail_->last >= 10){
         Item* item2 = new Item();
-        // do I have to dynamically alocate this Item* as well?
         Item* t_copy = tail_; 
-        //t_copy = tail_;
-        // if (tail_ == head_){
-        //   head_ = t_copy;
-        //   t_copy->prev = NULL;
-        // }
-        
         t_copy->next = item2;
         tail_ = item2;
+      
         tail_->prev = t_copy;
-        // how do I make the previous tail nodes next point to new node
         // woudl I have to do deep copy, 
-        //t_copy->next = item2;
         tail_->val[0] = val;
         tail_->first = 0;
         tail_->last = 1;
         tail_->next = NULL;
         size_ = size_ + 1;
-        //delete item2;
-        //delete t_copy;
       }
       else{
         tail_->val[tail_->last] = val;
@@ -126,33 +93,30 @@ void ULListStr::push_back(const std::string& val){
 
 void ULListStr::push_front(const std::string& val){
   if (head_ == NULL){
-    Item* item_1 = new Item();
-    head_ = item_1;
-    tail_ = item_1;
+    Item* item23 = new Item();
+    head_ = item23;
+    tail_ = item23;
+    std::string temp = val;
     // do I need to redefine ARRSIZE in this file, thought it was global in .h imported file
-    item_1->val[10-1] = val;
-    item_1->first = 10-1;
-    item_1->last = 10;
-    item_1->prev = NULL;
-    item_1->next = NULL;
+    head_->val[10-1] = temp;
+    head_->first = 10-1;
+    head_->last = 10;
+    head_->prev = NULL;
+    head_->next = NULL;
     size_ = 1;
-    //delete item_1;
   }
-
   else{
-    if ( !(head_->val[head_->first-1].empty())) {
+    if ( head_->first <= 0) {
       Item* item3 = new Item();
-      // should I do deep copy to like Item(head_) or something else with dynamic memory?
       Item* h_copy = head_;
       h_copy->prev = item3;
       head_ = item3;
-      item3->first = 9;
-      item3->last = 10;
-      item3->prev = NULL;
-      item3->next = h_copy;
-      item3->val[10-1] = val;
+      head_->next = h_copy;
+      head_->val[10-1] = val;
+      head_->first = 9;
+      head_->last = 10;
+      head_->prev = NULL;
       size_ = size_ + 1;
-      //delete item3;
     }
     else{
       head_->val[head_->first - 1] = val;
@@ -167,12 +131,19 @@ void ULListStr::pop_back(){
     throw std::invalid_argument("Bad location");
   }
   else{
-    //tail_->val[tail_->last-1] = " ";
-    if (tail_->last >= 1){
-      tail_->last = tail_->last - 1;
+    if ( (tail_->last - tail_->first) == 1 ){
+      Item* temp = tail_->prev;
+      //delete tail_;
+      tail_ = temp;
     }
-    if (size_ >= 1){
-      size_ = size_ - 1;
+    else {
+    //tail_->val[tail_->last-1] = " ";
+      if (tail_->last >= 1){
+        tail_->last = tail_->last - 1;
+      }
+      if (size_ >= 1){
+        size_ = size_ - 1;
+      }
     }
   }
 }
@@ -182,17 +153,25 @@ void ULListStr::pop_front(){
     throw std::invalid_argument("Bad location");
   }
   else {
-    head_->first = head_->first + 1;
-    // how to deal with head->first going over
-    if (size_ >= 1){
-      size_ = size_ - 1;
-    }   
+    if ( (head_->last - head_->first) == 1) {
+      Item* temp_h = head_->next;
+      delete head_;
+      head_ = temp_h;
+    }
+    else {
+      head_->first = head_->first + 1;
+      // how to deal with head->first going over
+      if (size_ >= 1){
+        size_ = size_ - 1;
+      }
+    }
   }
 }
 
 std::string const & ULListStr::back() const {
   // std::string* ptr;
   // ptr = NULL;
+  // call delete on tail_ when calling pop_back edge case
   if(tail_== NULL){
     //throw exception
     throw std::invalid_argument("Bad location");
--- ulliststr_test.cpp
+++ ulliststr_test.cpp
@@ -6,43 +6,121 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
+  // push to front example 1
   ULListStr trial;
+  trial.push_front("1");
+  for(int i = 0; i < 8; i++){
+    trial.push_front("3");
+  }
+  cout << "printing size expecting 9: " << trial.size() << endl;
+  cout << "expecting 3: " << trial.get(0) << endl;
+  cout << "expecting 1: " << trial.get(8) << endl;
+
+  //example 2 creating new node push_back 
+  ULListStr test1;
+  test1.push_back("1");
+  test1.push_front("7");
+  cout << "should print 7: " << test1.get(0) << endl;
+  cout << "should print 1: " << test1.get(1) << endl;
+  cout << "size shold be 2: " << test1.size();
+  test1.pop_back();
+  cout << "should print 7 " << test1.get(0) << endl;
+
+
+//example 3 creating new node push_front then push_back then pop front original node to make push_back only node
+  ULListStr test4;
+  test4.push_front("5");
+  test4.push_back("4");
+  cout << "should print 5: " << test4.get(0) << endl;
+  cout << "should print 4: " << test4.get(1) << endl;
+  test4.pop_front();
+  cout << "should print 4: " << test4.get(0) << endl;
+
+
+// example edge case with empty
+  try{
+    ULListStr test;
+    test.get(0);
+  }
+  catch(exception& t){
+    cout << t.what() <<endl;
+  }
+
+
+
+
+
+  
+
+
+  
+
+
 
   
   
-  //EXAMPLE THAT I NEED HELP JUST DOUBLE CHECKING I AM CORRECT.
+  //EXAMPLE 1
+  // cout << "before push front"  << endl;
   // trial.push_front("1");
-  // cout<<"line 15"<<endl;
+  // cout << "after push front" << endl;
   // cout << trial.get(0) << endl;
+  // cout << "before push back " << endl;
   // trial.push_back("6");
+  // cout << "after push back" << endl;
+  // cout << "should be 1 " << trial.get(0) << endl;
+  // cout << "should be 6 " << trial.get(1) << endl;
+  // // trial.pop_back();
+  // // string const& temp = trial.back();
+  // // cout << "should be 1" << temp;
+
+
+  //Example 1A
+  // trial.push_back("1");
+  // trial.push_front("7");
+  // cout << "shoudl print 7 " << trial.get(0) << endl;
+  // cout << "shoudl print 1 " << trial.get(1) << endl;
+  // trial.pop_front();
+  // cout << "should print 1 " << trial.get(0) << endl;
+  // //cout << "should crash " << trial.get(1) << endl; // put in tyr and catch
+
+  
+
+
+
+
+
+
+  
+  
+  
+
+
+
+  /// Example 2 - works as of now
+  // trial.push_back("6");
+  // trial.push_back("2");
+  // //trial.push_front("3");
+
   // cout << trial.get(0) << endl;
   
+  // string const& temp = trial.back();
+  // cout << "back should be 2 " << temp << endl;
+
+  // string const& temp1 = trial.front();
+  // cout << "front should be 6 " << temp1 << endl;
 
 
-  trial.push_back("6");
-  trial.push_back("2");
-  //trial.push_front("3");
-
-  cout << trial.get(0) << endl;
+  // // cout << trial.get(0) <<  " " << trial.get(1) << " " << trial.get(2) << " " << endl;
+  // cout << " should be 6: " << trial.get(0) <<  " should be 2: " << trial.get(1) << endl;
+  // trial.pop_back(); //should remove 2
+  // cout << "should be 6: "<< trial.get(0) << endl;
+  // trial.push_front("5");
+  // cout << "should be 5: " << trial.get(0) << " should be 6: " << trial.get(1) << endl;
   
-  string const& temp = trial.back();
-  cout << "back" << temp << endl;
-
-  string const& temp1 = trial.front();
-  cout << "front" << temp1 << endl;
-
-
-  // cout << trial.get(0) <<  " " << trial.get(1) << " " << trial.get(2) << " " << endl;
-  cout << trial.get(0) <<  " " << trial.get(1) << endl;
-  trial.pop_back();
-  // cout << trial.get(0) <<  " " << trial.get(1) << endl;
-  // trial.pop_front();
   
   
 
  
-  cout << trial.get(0) << endl;
-  cout << trial.size() << endl;
 
   
   
