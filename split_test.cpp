43843ae25a4edacafa66d853309deb55c6321318
Sidhanth Kapila <skapila@usc.edu>
2022-09-09 22:55:07 +0000

    s

--- split_test.cpp
+++ split_test.cpp
@@ -5,9 +5,11 @@
 using namespace std;
 
 int main(int argc, char* argv[]){
-  struct Node* temp2 = new Node(-17,nullptr);
+
+  // first test
+  struct Node* temp2 = new Node(3,nullptr);
   struct Node* temp1 = new Node(2,nullptr);
-  struct Node* temp = new Node(1,nullptr);
+  struct Node* temp = new Node(-1,nullptr);
   temp->next = temp1;
   temp1->next = temp2;
   struct Node* evens = NULL;
@@ -17,14 +19,41 @@ int main(int argc, char* argv[]){
 
   split(in,odds,evens);
   while(odds != nullptr){
-    cerr << "value: " << odds->value << " ";
+    cerr << "odd value: " << odds->value << " " << endl;
     odds = odds->next;
   }
+  while (evens != nullptr){
+    cerr << "even value: " << evens->value << " " << endl;
+    evens = evens->next;
+  }
+
+  // test 2 empty Node
+  struct Node* test = NULL;
+  struct Node* evens2 = NULL;
+  struct Node* odds2 = NULL;
+  Node* pass = test;
+  split(pass,odds2,evens2);
+  while(evens2 != nullptr){
+    cerr << "even value: " << evens2->value << " " << endl;
+    evens2 = evens2->next;
+  }
+  while(odds2 != nullptr){
+    cerr << "odd value: " << odds2->value << " " << endl;
+    odds2 = odds2->next;
+  }
+  if (odds2 == nullptr && evens2 == nullptr){
+    cerr << "empty null linked list passed in" << endl;
+  }
+  
+ 
+
 
 
   delete temp2;
   delete temp1;
   delete temp;
+
+
   
 
 
